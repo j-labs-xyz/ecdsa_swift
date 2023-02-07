208792d0888d9ac8ab4311624205ad236d593f12
leven <isralleven@gmail.com>
2023-02-07 23:43:37 +0800

    - folder update

--- Example/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-Example/Pods-Example-frameworks.sh
@@ -177,11 +177,9 @@ code_sign_if_enabled() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Libecdsa_swift/Libecdsa_swift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Zip/Zip.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Libecdsa_swift/Libecdsa_swift.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/Zip/Zip.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
